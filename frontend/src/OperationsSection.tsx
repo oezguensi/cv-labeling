a0951262e650ac5c08ae6e15a645e28f0c836780
oezguensibm <Sinan.Oezguen@ibm.com>
2023-02-14 10:36:33 +0100

    use useMemo to calculate activeOperations

--- frontend/src/OperationsSection.tsx
+++ frontend/src/OperationsSection.tsx
@@ -1,11 +1,13 @@
 import { Column } from '@carbon/react'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import ActiveOperationsList from "./ActiveOperationsList"
 import OperationsList from "./OperationsList"
 
 const OperationsSection = () => {
     const [operations, setOperations] = useState<any[]>([])
     const [activeOperationIDs, setActiveOperationIDs] = useState<string[]>([])
+
+    const activeOperations = useMemo(() => operations.filter(operation => activeOperationIDs.includes(operation.id)), [operations, activeOperationIDs])
 
     useEffect(() => {
         setOperations([{ id: '1', title: 'test', description: 'testdesc' }, { id: '2', title: 'test2', description: 'testdesc2' }, { id: '3', title: 'test3', description: 'testdesc3' }, { id: '4', title: 'test4', description: 'testdesc4' }])
@@ -18,7 +20,7 @@ const OperationsSection = () => {
     return (
         <>
             <Column lg={5}><OperationsList operations={operations} setActiveOperationIDs={setActiveOperationIDs} /></Column>
-            <Column lg={1}><ActiveOperationsList activeOperations={operations.filter(operation => activeOperationIDs.includes(operation.id))} /></Column>
+            <Column lg={1}><ActiveOperationsList activeOperations={activeOperations} /></Column>
         </>
     )
 }
